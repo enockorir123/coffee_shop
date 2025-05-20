ec1ebc788ee541b1c05dcb8383ef82cd900066c4
Enock Korir <enockorir123@gmail.com>
2025-05-20 16:57:16 +0300

    add relationship methods to Customer and Coffee classes

--- coffee.py
+++ coffee.py
@@ -1,7 +1,10 @@
 
+from order import Order
+from customer import Customer
+
 class Coffee:
     def __init__(self, name: str):
-        self.name = name  
+        self.name = name  # validated in setter
 
     @property
     def name(self) -> str:
@@ -13,3 +16,30 @@ class Coffee:
             self._name = value
         else:
             raise ValueError("Coffee name must be a string with at least 3 characters.")
+
+    def orders(self):
+        """
+        Return a list of Order instances for this coffee.
+        """
+        return [order for order in Order._all_orders if order.coffee is self]
+
+    def customers(self):
+        """
+        Return a unique list of Customer instances who have ordered this coffee.
+        """
+        return list({order.customer for order in self.orders()})
+
+    def num_orders(self) -> int:
+        """
+        Total times this coffee has been ordered.
+        """
+        return len(self.orders())
+
+    def average_price(self) -> float:
+        """
+        Average price paid for this coffee.
+        """
+        orders = self.orders()
+        if not orders:
+            return 0.0
+        return sum(order.price for order in orders) / len(orders)
--- customer.py
+++ customer.py
@@ -1,7 +1,10 @@
 
+from order import Order
+from coffee import Coffee
+
 class Customer:
     def __init__(self, name: str):
-        self.name = name  # use setter for validation
+        self.name = name  # validated in setter
 
     @property
     def name(self) -> str:
@@ -13,3 +16,39 @@ class Customer:
             self._name = value
         else:
             raise ValueError("Name must be a string between 1 and 15 characters.")
+
+    def orders(self):
+        """
+        Return a list of Order instances for this customer.
+        """
+        return [order for order in Order._all_orders if order.customer is self]
+
+    def coffees(self):
+        """
+        Return a unique list of Coffee instances this customer has ordered.
+        """
+        # Use a set to dedupe, then return as list
+        return list({order.coffee for order in self.orders()})
+
+    def create_order(self, coffee: Coffee, price: float):
+        """
+        Create a new Order linking this customer to a coffee at given price.
+        """
+        return Order(self, coffee, price)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        """
+        Return the Customer who has spent the most on the given coffee.
+        If no orders for that coffee, return None.
+        """
+        # Build a mapping of customer -> total spent
+        spend = {}
+        for order in Order._all_orders:
+            if order.coffee is coffee:
+                spend.setdefault(order.customer, 0.0)
+                spend[order.customer] += order.price
+        if not spend:
+            return None
+        # Return the customer with max total
+        return max(spend, key=spend.get)
